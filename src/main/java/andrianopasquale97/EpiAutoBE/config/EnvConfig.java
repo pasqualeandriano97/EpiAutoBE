c58374d6e0409bb3fc33e6a4d8ea07227e431287
pasqualeandriano97 <151465437+pasqualeandriano97@users.noreply.github.com>
2024-05-26 01:11:28 +0200

    Docker file add

--- src/main/java/andrianopasquale97/EpiAutoBE/config/EnvConfig.java
+++ src/main/java/andrianopasquale97/EpiAutoBE/config/EnvConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource(value = "file:./env.properties", ignoreResourceNotFound = true)
+@PropertySource(value = "file:./resources/application.properties", ignoreResourceNotFound = true)
 public class EnvConfig {
 }
